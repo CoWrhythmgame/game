df8bfc017c99faf2a1cd1c60d54a4acd5c3b5120
rew1220 <103731244+rew1220@users.noreply.github.com>
2026-06-27 16:19:52 +0900

    테스트 파일 추가

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public int testint = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
