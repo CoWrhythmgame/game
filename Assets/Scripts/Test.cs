41f2c5dfe0b83be5e8fd5dd4d3732ab1bea8c214
rew1220 <103731244+rew1220@users.noreply.github.com>
2026-06-27 16:39:36 +0900

    머지 테스트2

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Test : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public int test2int = 0;
+    public int test3int = 0;
     void Start()
     {
         
